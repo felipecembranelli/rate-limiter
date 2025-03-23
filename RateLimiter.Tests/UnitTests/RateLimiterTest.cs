2e1d9bc5e0d591ef4b3a2e74af1a381dc4e82b00
Felipe Cembranelli <felipecembranelli@live.com>
2025-03-23 07:50:11 -0700

    initial commit

---
+++ RateLimiter.Tests/UnitTests/RateLimiterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace RateLimiter.Tests.UnitTests;
+
+[TestFixture]
+public class RateLimiterTest
+{
+    [Test]
+    public void Example()
+    {
+        Assert.That(true, Is.True);
+    }
+}
